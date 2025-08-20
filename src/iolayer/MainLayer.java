b69c8c68a2523e74fa6f29e8421809dface9141c
WhiteBellbird <cloudokh@gmail.com>
2025-08-20 17:17:08 +0900

    modify controller,iolayer package

--- src/iolayer/MainLayer.java
+++ src/iolayer/MainLayer.java
@@ -1,5 +1,17 @@
 package iolayer;
 
-public class MainLayer {
+import java.util.Scanner;
 
+public class MainLayer {
+    /**
+     *  1. 회원가입
+     2. 로그인
+     3. 상품 둘러보기
+     4. 프로그램 종료
+     */
+    private Scanner scanner;
+
+    public MainLayer(Scanner scanner) {
+        this.scanner = scanner;
+    }
 }
---
+++ src/iolayer/OrderIOLayer.java
@@ -0,0 +1,12 @@
+package iolayer;
+
+import java.util.Scanner;
+
+public class OrderIOLayer {
+
+    private Scanner scanner;
+
+    public OrderIOLayer(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
