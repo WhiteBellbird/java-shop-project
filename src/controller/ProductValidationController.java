96b41d46f8558cef7ca5b81fe824abdfdc10809f
lhj000321 <gothf108@gamail.com>
2025-08-23 21:23:07 +0900

    feat: 상품 기능 수정 및 입력 유효성 추가

--- src/controller/ProductValidationController.java
+++ src/controller/ProductValidationController.java
@@ -1,10 +1,45 @@
 package controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import domain.Product;
+import exception.ShopException;
+import service.ProductService;
+
 public class ProductValidationController {
+    private ProductService productService;
+
+    public ProductValidationController(ProductService productService) {
+        this.productService = productService;
+    }	
 	public boolean validateCreateProduct(String productId, String name, String category, int price, int stock, String description) {
 		if(productId.isEmpty() || name.isEmpty() || category.isEmpty() || price <= 0 || stock <= 0 || description.isEmpty()) {
 			return false;
 		}
 		return true;
 	}
+    public void addProduct(String productId, String name, String category, int price, int stock, String description) throws ShopException {
+        if (!validateCreateProduct(productId, name, category, price, stock, description)) {
+            throw new ShopException("모든 상품 정보를 정확히 입력해야 합니다.");
+        }
+        Product product = new Product(productId, name, category, price, stock, description, LocalDateTime.now());
+        productService.addProduct(product);
+    }
+    public List<Product> getAllProducts() throws ShopException {
+        return productService.getAllProducts();
+    }
+    public Product getProductById(String productId) throws ShopException {
+        return productService.getProductById(productId);
+    }
+    public void updateProduct(Product product) throws ShopException {
+        productService.updateProduct(product);
+    }
+    public void deleteProduct(String productId) throws ShopException {
+        productService.deleteProduct(productId);
+    }
+    public void reduceStock(String productId, int quantity) throws ShopException {
+        productService.reduceStockByProductId(productId, quantity);
+    }
+	
 }
